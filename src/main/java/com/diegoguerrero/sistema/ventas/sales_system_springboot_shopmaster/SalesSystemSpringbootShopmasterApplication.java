b39ecdaf613011cde6306a91862eca3743b02d28
Diego Fernando Guerrero Perez <diego.guerrero9612@gmail.com>
2024-09-29 21:39:48 -0500

    Primer Commit del proyecto. Se creo el proyecto y se agregaron configuraciones en el archivo application.properties de ajustes varios

---
+++ src/main/java/com/diegoguerrero/sistema/ventas/sales_system_springboot_shopmaster/SalesSystemSpringbootShopmasterApplication.java
@@ -0,0 +1,13 @@
+package com.diegoguerrero.sistema.ventas.sales_system_springboot_shopmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSystemSpringbootShopmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesSystemSpringbootShopmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegoguerrero/sistema/ventas/sales_system_springboot_shopmaster/SalesSystemSpringbootShopmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegoguerrero.sistema.ventas.sales_system_springboot_shopmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesSystemSpringbootShopmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
